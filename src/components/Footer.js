aac12dc03304a6abfce1747c7fbf1c2fffa45217
Pankaj Kulshreshtha <kulshresthapankaj@gmail.com>
2020-03-30 19:18:58 +0530

    make it better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,20 +7,3 @@ export default ()=>(
            <p>All Rights Reserved.</p>
         </footer>
 );
-
-// import react,{Component,useState} from 'react';
-
-// let counter=()=>{
-// const {counter,setCounter} =useState(0)
-
-//   render(){
-//       return(
-//           <div>
-//           <div>{this.state.counter}</div>
-//           <button onclick={setCounter(counter++)}>+</button>
-//           <button onclick={setCounter(counter--)} >-</button>
-//           </div>
-//       )
-//   }
-
-// }
