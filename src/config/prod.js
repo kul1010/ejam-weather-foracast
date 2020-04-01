903bc277a1301e3939c7bc2ebee27ced3d282f93
Pankaj Kulshreshtha <kulshresthapankaj@gmail.com>
2020-04-01 18:40:27 +0530

    change process env

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,4 +1,4 @@
-console.log('tttt',process.env)
+import {} from 'dotenv/config'
 export const keys = {
     "yahooAppId": process.env.YAHOO_APP_ID,
     "yahooConsumerKey": process.env.YAHOO_CONSUMER_KEY,
